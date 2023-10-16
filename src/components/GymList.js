9495564a28edbe5ecac63a535b6baa6bf1ccfa48
Vikash Kumar <vikasthakur9310@gmail.com>
2023-10-16 23:54:45 +0530

    add

--- src/components/GymList.js
+++ src/components/GymList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./GymList.css";
 // import DistanceFilter from "./DistanceFilter";
 // import FeaturesFilter from "./FeaturesFilter";
 // import LocationFilter from "./LocationFilter";
@@ -6,25 +7,37 @@ import React from "react";
 
 // import GymItem from './GymItem';
 export const GymList = ({ gyms }) => {
-   
-   
-
-   
-
-    return (
-        <div className="gym_list">
-            <div>
-                {/* <LocationFilter/> */}
-                {/* <CategoryFilter products={gyms}/> */}
-                {/* <FeaturesFilter /> */}
-                {/* <DistanceFilter /> */}
+  return (
+    <div className="gym_list">
+      <div>
+        {/* <LocationFilter/> */}
+        {/* <CategoryFilter products={gyms}/> */}
+        {/* <FeaturesFilter /> */}
+        {/* <DistanceFilter /> */}
+      </div>
+      <div>
+        {gyms.map((gym) => (
+          <div key={gym.id} className="gym_item">
+            <img
+              src="https://i.pinimg.com/564x/c1/da/58/c1da586510db186fa1af573219c224cd.jpg"
+              alt="image"
+              className="gym_image"
+            />
+            <div className="gym_info">
+              <h2>{gym.gym_name}</h2>
+              <p>
+                {gym.address1}, {gym.address2}
+              </p>
+              <p>
+                {gym.city}, {gym.country}
+              </p>
+              <p>{gym.description}</p>
+              <p>Distance: {gym.distance} miles</p>
             </div>
-            <div>
-                {gyms.map((gym) => (
-                    <li>{gym.gym_name} , {gym.address1}, {gym.address2}, {gym.city}, {gym.country}, {gym.description},{gym.distance},{gym.cover_image},{gym.gallery.images}</li>
-                ))}
-            </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default GymList;
